263f2ac01d64623bbd0db32ff17c1c127229e09c
Thiago Bignotto <bignotto@gmail.com>
2019-03-15 03:18:39 +0000

    lista os arquivos xml de uma pasta

---
+++ robot.js
@@ -0,0 +1,30 @@
+var fs = require('fs')
+var _config = require('./config')
+
+function start() {
+    const content = {}
+
+    content.files = getListaArquivosXml()
+    console.log(content)
+
+    function getListaArquivosXml() {
+        //array com os nomes dos arquivos para upload
+        var xmlFiles = [];
+
+        //pattern para extensão do arqivo
+        var pattern=/\.[0-9a-z]+$/i;
+        try {
+            xmlFiles = fs.readdirSync('/mnt/c/xml/nao_processado')
+        } catch (err) {
+            console.error(err)
+        }
+        
+        //keep only files with .XML extension
+        return xmlFiles.filter(function(fileName) {
+            var ext = fileName.match(pattern);
+            if(ext.toString().toLowerCase() == '.xml') return true
+            else false
+        })
+    }
+}
+start()
